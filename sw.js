31c1e5489ce9cf2ff9e7b58425db230a5f694ec7
djjohnsongeek <danieleejohnson@gmail.com>
2024-10-14 18:44:20 -0400

    feat: bump version

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const GIT_HUB_PATH = "/Q-AND-A-PWA"
-const VERSION = 'v0.1.8';
+const VERSION = 'v0.1.9';
 const CACHE_NAME = `question-answer-${VERSION}}`;
 const EXPECTED_CACHES = [CACHE_NAME];
 const CACHED_RESOURCES = [
